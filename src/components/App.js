acd314769a75c2d12a0233b5747d03dd25a2f126
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-30 21:24:28 +0200

    add fontawsome

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ListItems from "./ListItems";
 import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrash);
 
 class App extends React.Component {
   state = {
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Item.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Item = (props) => {
   return (
     <div className="list-item">
-      <p>{props.item.text}</p>
+      <p>
+        {props.item.text}{" "}
+        <span>
+          <FontAwesomeIcon className="faicons" icon="trash" />
+        </span>
+      </p>
     </div>
   );
 };
