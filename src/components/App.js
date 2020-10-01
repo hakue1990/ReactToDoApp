8c6e345f0a35d349681ae549700dd545ff713ab2
Adam Hałdaś <adam.haldas@gmail.com>
2020-10-01 19:56:34 +0200

    change all the stuffs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ListItems from "./ListItems";
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
@@ -7,52 +6,10 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 library.add(faTrash);
 
 class App extends React.Component {
-  state = {
-    tasks: [],
-    currentTask: {
-      text: "",
-      key: "",
-    },
-  };
-  handleInput = (e) => {
-    this.setState({
-      currentTask: {
-        text: e.target.value,
-        key: Date.now(),
-      },
-    });
-  };
-  addItem = (e) => {
-    e.preventDefault();
-    const newTask = this.state.currentTask;
-    if (newTask.text !== "") {
-      const items = [...this.state.tasks, newTask];
-      this.setState({
-        tasks: items,
-        currentTask: {
-          text: "",
-          key: "",
-        },
-      });
-    }
-  };
+  state = {};
+
   render() {
-    return (
-      <header>
-        <div className="App">
-          <form className="to-do-form" onSubmit={this.addItem}>
-            <input
-              type="text"
-              placeholder="enter text"
-              value={this.state.currentTask.text}
-              onChange={this.handleInput}
-            />
-            <button type="submit">Add Task</button>
-            <ListItems items={this.state.tasks} />
-          </form>
-        </div>
-      </header>
-    );
+    return <h1>change all concept</h1>;
   }
 }
 
